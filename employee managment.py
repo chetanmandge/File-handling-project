17d36bb52709455c22bbd14fa1ea1fd87fc54423
chetanmandge <chetanmandge05@gmail.com>
2022-02-15 19:48:32 +0530

    new file added

--- employee managment.py
+++ employee managment.py
@@ -1,8 +1,10 @@
+import os
 def add_employee():
-    with open("employee_db.text") as fp:
+    with open("employee_db.text","a") as fp:
         name=input("enter employee name:-")
         Eid=input("enter employee ID :-")
         dept = input(" enter employee department :-")
         fp.write(f"{name}~{Eid}~{dept}~\n")
         print(f"employee {name} added successfully")
 
+add_employee()
